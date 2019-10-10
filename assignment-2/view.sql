00c7c53cb8248bab61353db8d9e1a60d7f09b219
Yiwei Wang <wang.yiw@husky.neu.edu>
2019-10-09 21:50:23 -0400

    final version

--- assignment-2/view.sql
+++ assignment-2/view.sql
@@ -1,5 +1,3 @@
-DROP VIEW IF EXISTS `developer_roles_and_privileges`;
-
 create view developer_roles_and_privileges as
 select per.`first_name`, per.`last_name`, per.`username`, per.`email`,
 	   web.`name` as `website_name`, web.`visits` as `website_visits`, web.`updated` as `website_updated_date`, -- in case update duplicate
@@ -7,10 +5,11 @@ select per.`first_name`, per.`last_name`, per.`username`, per.`email`,
 	   pg.`title` as `page_title`, pg.`views`, pg.`updated`as `page_updated_date`,-- in case update duplicate
        pg_r.`role` as `page_role`, pg_p.`priviledge` as `page_priviledge`
 from
-	   `person`per, 
-       `website`web, 
-       `website_role`web_r, `website_priviledge`web_p,
-       `page`pg, 
-       `page_role`pg_r, `page_priviledge`pg_p,
-       `developer` dev
-where per.id in (dev.person_id);
+	   `person`per join `developer` dev on per.id = dev.person_id
+				   join `website_role`web_r on dev.person_id = web_r.developer_id
+                   join `website`web on web_r.website_id = web.id
+                   join `website_priviledge` web_p on web_p.developer_id = dev.person_id and web_p.website_id = web.id
+				   left join `page` pg on web.id = pg.website_id
+                   left join `page_role` pg_r on pg.id = pg_r.page_id and dev.person_id = pg_r.developer_id
+                   left join `page_priviledge` pg_p on pg.id = pg_p.page_id and dev.person_id = pg_p.developer_id;
+				
